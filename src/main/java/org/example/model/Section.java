8184fc118909b52e5d8c786337c6ccde858e66cc
Paul Harris E. Bathan <icecreamloverr28@gmail.com>
2026-04-23 23:23:07 +0800

    Created class Section

---
+++ src/main/java/org/example/model/Section.java
@@ -0,0 +1,46 @@
+package org.example.model;
+
+public class Section {
+    private int sectionIndex;
+    private String sectionName;
+    private int yearLevel;
+
+
+    public Section (int sectionIndex, String sectionName, int yearLevel){
+        this.sectionIndex = sectionIndex;
+        this.sectionName = sectionName;
+        this.yearLevel = yearLevel;
+    }
+    public int getSectionIndex() {
+        return sectionIndex;
+    }
+
+    public void setSectionIndex(int sectionIndex) {
+        this.sectionIndex = sectionIndex;
+    }
+
+    public String getSectionName() {
+        return sectionName;
+    }
+
+    public void setSectionName(String sectionName) {
+        this.sectionName = sectionName;
+    }
+
+    public int getYearLevel() {
+        return yearLevel;
+    }
+
+    public void setYearLevel(int yearLevel) {
+        this.yearLevel = yearLevel;
+    }
+
+    @Override
+    public String toString() {
+        return "Section{" +
+                "sectionIndex=" + sectionIndex +
+                ", sectionName='" + sectionName + '\'' +
+                ", yearLevel=" + yearLevel +
+                '}';
+    }
+}
