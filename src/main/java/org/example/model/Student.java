c80df7b870a8a37a90fcc1bf6aeb322554bb6bf6
Paul Harris E. Bathan <icecreamloverr28@gmail.com>
2026-04-24 12:03:22 +0800

    Removed variable program from Class Student

--- src/main/java/org/example/model/Student.java
+++ src/main/java/org/example/model/Student.java
@@ -1,27 +1,15 @@
 package org.example.model;
 
 public class Student extends Person {
-    private String program;
-
     public Student(int index, String id, String name, String program) {
         super.setIndex(index);
         super.setId(id);
         super.setName(name);
-        this.program = program;
-    }
-
-    public String getProgram() {
-        return program;
-    }
-
-    public void setProgram(String program) {
-        this.program = program;
     }
 
     public void display(){
         System.out.println("Student ID: " + super.getId());
         System.out.println("Student Name: " + super.getName());
-        System.out.println("Program: " + getProgram());
         System.out.println();
     }
 
@@ -30,10 +18,4 @@ public class Student extends Person {
         System.out.println("Student Studies");
     }
 
-    @Override
-    public String toString() {
-        return "Student{" +
-                "program='" + program + '\'' +
-                '}';
-    }
 }
