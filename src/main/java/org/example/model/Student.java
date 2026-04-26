af2dac9fb6b4c891709aa5d8e8d6d5efacfc0fa8
Paul Harris E. Bathan <icecreamloverr28@gmail.com>
2026-04-26 11:13:49 +0800

    Added implementation for Student Service, updated display for student, and rafactored updateStudent in Interface Student Service

--- src/main/java/org/example/model/Student.java
+++ src/main/java/org/example/model/Student.java
@@ -1,16 +1,14 @@
 package org.example.model;
 
 public class Student extends Person {
-    public Student(int index, String id, String name, String program) {
+    public Student(int index, String id, String name) {
         super.setIndex(index);
         super.setId(id);
         super.setName(name);
     }
 
     public void display(){
-        System.out.println("Student ID: " + super.getId());
-        System.out.println("Student Name: " + super.getName());
-        System.out.println();
+        System.out.printf("Index: [%d] | Student ID: %s | Student Name: %s %n", super.getIndex(), super.getId(), super.getName());
     }
 
     @Override
---
+++ src/main/java/org/example/service/StudentServiceImpl.java
@@ -0,0 +1,48 @@
+package org.example.service;
+
+import org.example.model.Student;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StudentServiceImpl implements IStudentService{
+    private final Scanner scanner = new Scanner(System.in);
+    //holds all student objects accessible via index
+    //if to add a student to section place index
+    private final ArrayList<Student> studentList = new ArrayList<>();
+
+    public void addStudent(Student student){
+        studentList.add(student);
+    }
+
+    public void displayAllStudent(){
+        for (Student student : studentList) {
+            student.display();
+        }
+    }
+
+    public void updateStudent(int index){
+        for (int i = 0; i < studentList.size(); i++){
+            // matches the student details in the list with the received student data
+            if(studentList.get(i).getIndex() == index) {
+                System.out.println("Enter Name: ");
+                String studentName = scanner.nextLine();
+
+                System.out.println("Enter Student ID: ");
+                String studentId = scanner.nextLine();
+
+                studentList.set(i, new Student(index, studentId, studentName));
+                break;
+            }
+        }
+    }
+
+    public void removeStudent(Student student){
+        for (int i = 0; i < studentList.size(); i++){
+            if(studentList.get(i).getId().equals(student.getId())){
+                studentList.remove(i);
+                break;
+            }
+        }
+    }
+}
