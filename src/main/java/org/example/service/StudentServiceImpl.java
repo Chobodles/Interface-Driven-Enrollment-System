d40dc0192d267abf4d0617ac92819131e4de36ab
Paul Harris E. Bathan <icecreamloverr28@gmail.com>
2026-05-08 10:48:47 +0800

    Refactored student service implementation and interface. Removed all scanners to be moved to the controller

--- src/main/java/org/example/service/StudentServiceImpl.java
+++ src/main/java/org/example/service/StudentServiceImpl.java
@@ -4,43 +4,32 @@ import org.example.model.Student;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
-public class StudentServiceImpl implements IStudentService{
-    private final Scanner scanner = new Scanner(System.in);
-    //holds all student objects accessible via index
-    //if to add a student to section place index
+public class StudentServiceImpl implements IStudentService {
     private final List<Student> studentList = new ArrayList<>();
 
-    public void addStudent(Student student){
+    public void addStudent(Student student) {
         studentList.add(student);
     }
 
-    public void displayAllStudent(){
+    public void displayAllStudent() {
         for (Student student : studentList) {
             student.display();
         }
     }
 
-    public void updateStudent(int index){
-        for (int i = 0; i < studentList.size(); i++){
-            // matches the student details in the list with the received student data
-            if(studentList.get(i).getIndex() == index) {
-                System.out.println("Enter Name: ");
-                String studentName = scanner.nextLine();
-
-                System.out.println("Enter Student ID: ");
-                String studentId = scanner.nextLine();
-
-                studentList.set(i, new Student(index, studentId, studentName));
+    public void updateStudent(int index, String id, String name) {
+        for (int i = 0; i < studentList.size(); i++) {
+            if (studentList.get(i).getIndex() == index) {
+                studentList.set(i, new Student(index, id, name));
                 break;
             }
         }
     }
 
-    public void removeStudent(int index){
-        for (int i = 0; i < studentList.size(); i++){
-            if(studentList.get(i).getIndex() == index){
+    public void removeStudent(int index) {
+        for (int i = 0; i < studentList.size(); i++) {
+            if (studentList.get(i).getIndex() == index) {
                 studentList.remove(i);
                 break;
             }
