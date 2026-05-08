b415189cf9a6cb9570b66d1831a16b6059ec559d
Paul Harris E. Bathan <icecreamloverr28@gmail.com>
2026-05-08 10:51:46 +0800

    Refactor instructor service interface and implementation. Removed all scanners to be moved to the controller, and interface function parameters updated

--- src/main/java/org/example/service/InstructorServiceImpl.java
+++ src/main/java/org/example/service/InstructorServiceImpl.java
@@ -1,44 +1,39 @@
 package org.example.service;
 
+import org.example.model.Course;
 import org.example.model.Instructor;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
-public class InstructorServiceImpl implements IInstructorService{
+public class InstructorServiceImpl implements IInstructorService {
+    private final List<Instructor> instructorList = new ArrayList<>();
 
-    private final Scanner scanner = new Scanner(System.in);
-    private final List<Instructor> instructorList =new ArrayList<>();
-
-    public void addInstructor(Instructor instructor){
+    public void addInstructor(Instructor instructor) {
         instructorList.add(instructor);
     }
 
-    public void displayAllInstructor(){
+    public void displayAllInstructor() {
         for (Instructor instructor : instructorList) {
-            instructor.display();
-        }
-    }
-
-    public void updateInstructor(int index){
-        for (int i = 0; i < instructorList.size(); i++){
-            if(instructorList.get(i).getIndex() == index) {
-                System.out.println("Enter Instructor Name: ");
-                String instructorName = scanner.nextLine();
-
-                System.out.println("Enter Instructor ID: ");
-                String instructorId = scanner.nextLine();
-
-
-                instructorList.set(i, new Instructor(index, instructorId, instructorName, instructorList.get(i).getCourseList()));
+            List<Course> courses = instructor.display();
+            for (Course course : courses) {
+                course.display();
             }
         }
     }
 
-    public void removeInstructor(int index){
-        for (int i = 0; i < instructorList.size(); i++){
-            if(instructorList.get(i).getIndex() == index){
+    public void updateInstructor(int index, String id, String name) {
+        for (int i = 0; i < instructorList.size(); i++) {
+            if (instructorList.get(i).getIndex() == index) {
+                instructorList.set(i, new Instructor(index, id, name, instructorList.get(i).getCourseList()));
+                break;
+            }
+        }
+    }
+
+    public void removeInstructor(int index) {
+        for (int i = 0; i < instructorList.size(); i++) {
+            if (instructorList.get(i).getIndex() == index) {
                 instructorList.remove(i);
                 break;
             }
