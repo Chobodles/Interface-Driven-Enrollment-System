e4a8bdae505d5a8bd330c3980cb8ce76617a200d
Paul Harris E. Bathan <icecreamloverr28@gmail.com>
2026-05-09 00:27:30 +0800

    Added interface TuitionService

---
+++ src/main/java/org/example/service/ITuitionService.java
@@ -0,0 +1,10 @@
+package org.example.service;
+
+import org.example.model.Tuition;
+
+public interface ITuitionService {
+    double PRICE_PER_UNIT = 1000.00;
+
+    double calculateTuitionFee(Tuition tuition, int units, double discountRate);
+    void processPayment(Tuition tuition, double amount);
+}
