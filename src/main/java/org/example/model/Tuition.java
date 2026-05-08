d325ff5bd492e0c6fbfc367ae4653c232a1abff0
Paul Harris E. Bathan <icecreamloverr28@gmail.com>
2026-05-08 23:53:42 +0800

    Added model class Tuition to hold all tuition details which will be attached to each student

---
+++ src/main/java/org/example/model/Tuition.java
@@ -0,0 +1,50 @@
+package org.example.model;
+
+public class Tuition {
+    private double total;
+    private double totalAmountPaid;
+    private double remainingBalance;
+    private boolean isFullyPaid;
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public double getTotalAmountPaid() {
+        return totalAmountPaid;
+    }
+
+    public void setTotalAmountPaid(double totalAmountPaid) {
+        this.totalAmountPaid = totalAmountPaid;
+    }
+
+    public double getRemainingBalance() {
+        return remainingBalance;
+    }
+
+    public void setRemainingBalance(double remainingBalance) {
+        this.remainingBalance = remainingBalance;
+    }
+
+    public boolean isFullyPaid() {
+        return isFullyPaid;
+    }
+
+    public void setFullyPaid(boolean fullyPaid) {
+        isFullyPaid = fullyPaid;
+    }
+
+    @Override
+    public String toString() {
+        return "Tuition{" +
+                "total=" + total +
+                ", totalAmountPaid=" + totalAmountPaid +
+                ", remainingBalance=" + remainingBalance +
+                ", isFullyPaid=" + isFullyPaid +
+                '}';
+    }
+}
