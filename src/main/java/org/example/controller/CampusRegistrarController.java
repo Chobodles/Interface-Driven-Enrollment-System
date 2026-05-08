b427ed53587561a2f08a2dcb01cc7f374a7d9380
Paul Harris E. Bathan <icecreamloverr28@gmail.com>
2026-05-08 23:31:45 +0800

    Added program functions to controller

--- src/main/java/org/example/controller/CampusRegistrarController.java
+++ src/main/java/org/example/controller/CampusRegistrarController.java
@@ -119,4 +119,26 @@ public class CampusRegistrarController {
         System.out.println("Department removed at index: " + index);
     }
 
+    //program
+
+    public void addProgram(Program program) {
+        programService.addProgram(program);
+        System.out.println("Program added: " + program.getProgramName());
+    }
+
+    public void displayAllPrograms() {
+        System.out.println("=== All Programs ===");
+        programService.displayAllProgram();
+    }
+
+    public void updateProgram(int index, String programId, String programName) {
+        programService.updateProgram(index, programId, programName);
+        System.out.println("Program updated at index: " + index);
+    }
+
+    public void removeProgram(int index) {
+        programService.removeProgram(index);
+        System.out.println("Program removed at index: " + index);
+    }
+
 }
