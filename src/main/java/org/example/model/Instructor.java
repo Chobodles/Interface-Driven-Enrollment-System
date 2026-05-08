0d0508a4b723838434f01cc5d90516f121b222a6
Paul Harris E. Bathan <icecreamloverr28@gmail.com>
2026-05-08 14:23:26 +0800

    Refactor model class instructor by removing classlist and rafactoring the instructor service implementation by changing the logic of the display  and update functions

--- src/main/java/org/example/model/Instructor.java
+++ src/main/java/org/example/model/Instructor.java
@@ -3,33 +3,25 @@ package org.example.model;
 import java.util.List;
 
 public class Instructor extends Person{
-    private final List<Course> courseList;
 
-    public Instructor(int index, String ID, String name, List<Course> courseList) {
+    public Instructor(int index, String ID, String name) {
         super.setIndex(index);
         super.setId(ID);
         super.setName(name);
-        this.courseList = courseList;
     }
 
     public void mainTask(){
         System.out.println("Instructor Grades");
     }
 
-    public List<Course> display(){
-        System.out.printf("Index: [%d] | Instructor ID: %s | Instructor Name: %s %n", super.getIndex(), super.getId(), super.getName());
-        return courseList.stream().toList();
-    }
-
-    public List<Course> getCourseList() {
-        return courseList;
+    public void display() {
+        System.out.printf("Index: [%d] | Instructor ID: %s | Instructor Name: %s %n",
+                super.getIndex(), super.getId(), super.getName());
     }
 
     @Override
     public String toString() {
-        return "Instructor{" +
-                "courseList=" + courseList +
-                '}';
+        return "Instructor{}";
     }
 }
 
