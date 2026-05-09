6438fa9916feb338466ea60f10aafb31c4393f34
Paul Harris E. Bathan <icecreamloverr28@gmail.com>
2026-05-09 13:45:44 +0800

    Added Controller for Loading Service

--- src/main/java/org/example/controller/CampusRegistrarController.java
+++ src/main/java/org/example/controller/CampusRegistrarController.java
@@ -55,61 +55,10 @@ public class CampusRegistrarController {
 
     //loading
 
-    public void addLoading(Loading loading) {
-        loadingService.addLoading(loading);
-        System.out.println("Loading added for course: " + loading.getCourse().getCourseName());
-    }
-
-    public void displayAllLoadings() {
-        System.out.println("=== All Loadings ===");
-        loadingService.displayAllLoading();
-    }
-
-    public void updateLoading(int index, Instructor instructor, Course course) {
-        loadingService.updateLoading(index, instructor, course);
-        System.out.println("Loading updated at index: " + index);
-    }
-
-    public void removeLoading(int index) {
-        loadingService.removeLoading(index);
-        System.out.println("Loading removed at index: " + index);
-    }
-
-    public void enrollStudentToLoading(int loadingIndex, Student student, List<Loading> allLoadings, double discountRate) {
-        try {
-            loadingService.enrollStudent(loadingIndex, student);
-            System.out.println("Student " + student.getName() + " enrolled to loading index: " + loadingIndex);
-            recalculateStudentTuition(student, allLoadings, discountRate);
-        } catch (LoadFullException e) {
-            System.out.println("Enrollment Error: " + e.getMessage());
-        }
-    }
-
-    public void removeStudentFromLoading(int loadingIndex, int studentIndex) {
-        loadingService.removeStudent(loadingIndex, studentIndex);
-        System.out.println("Student removed from loading index: " + loadingIndex);
-    }
 
     //tuition
 
-    private void recalculateStudentTuition(Student student, List<Loading> allLoadings, double discountRate) {
-        int count = 0;
-        for (Loading loading : allLoadings) {
-            if (loading.getStudentList().contains(student)) {
-                count++;
-            }
-        }
 
-        int totalUnits = (int) count * UNITS_PER_LOAD;
-
-        if (student.getTuition() == null) {
-            student.setTuition(new Tuition());
-        }
-
-        tuitionService.calculateTuitionFee(student.getTuition(), totalUnits, discountRate);
-        System.out.printf("Tuition recalculated for %s | Units: %d | Total: %.2f%n",
-                student.getName(), totalUnits, student.getTuition().getTotal());
-    }
 
     public void payStudentTuition(Student student, double amount) {
         if (student.getTuition() == null) {
