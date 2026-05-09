a74e3b9adeb2add017c3b95feb3442da7f199581
Paul Harris E. Bathan <icecreamloverr28@gmail.com>
2026-05-09 13:35:53 +0800

    Added Controller for Section Service

--- src/main/java/org/example/controller/CampusRegistrarController.java
+++ src/main/java/org/example/controller/CampusRegistrarController.java
@@ -53,26 +53,6 @@ public class CampusRegistrarController {
 
     //section
 
-    public void addSection(Section section) {
-        sectionService.addSection(section);
-        System.out.println("Section added: " + section.getSectionName());
-    }
-
-    public void displayAllSections() {
-        System.out.println("=== All Sections ===");
-        sectionService.displayAllSection();
-    }
-
-    public void updateSection(int index, String sectionName, int yearLevel) {
-        sectionService.updateSection(index, sectionName, yearLevel);
-        System.out.println("Section updated at index: " + index);
-    }
-
-    public void removeSection(int index) {
-        sectionService.removeSection(index);
-        System.out.println("Section removed at index: " + index);
-    }
-
     //loading
 
     public void addLoading(Loading loading) {
